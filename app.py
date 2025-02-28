748095a6d646095f6378fe9de7ab8be9c7f2338a
b12s307 <nicolas.ruiiz07@gmail.com>
2025-02-28 09:37:50 -0500

    create Invalidinterest and Invalidmonths classes and create exceptions

--- app.py
+++ app.py
@@ -1,2 +1,16 @@
+class Invalidinterest(Exception):
+    pass
+
+class Invalidmonths(Exception):
+    pass
+
+
 def calcular_ahorro_programado(monto: float, interes: float, periodo: int):
+
+    if interes < 0.1 or interes > 1:
+       raise Invalidinterest( "ERROR: La tasa de interes es invalida" )
+    
+    if periodo < 1:
+       raise Invalidmonths( "ERROR: El periodo es invalido" )
+
     return monto*(1+interes)*periodo-1/interes
